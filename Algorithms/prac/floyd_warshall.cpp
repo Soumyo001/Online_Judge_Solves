31ec1eb4335e7ac01c01107cf4b3e3a64d287254
soumyo001 <www.soumyo@gmail.com>
2025-08-09 23:15:31 +0600

    sols

--- Algorithms/prac/floyd_warshall.cpp
+++ Algorithms/prac/floyd_warshall.cpp
@@ -1,17 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e3+10;
-int graph[N][N];
+const int N = 500;
 int dist[N][N];
-bool vis[N][N];
 int n,m;
-//incomplete
 
 int main(void){
     cin>>n>>m;
     for(int i=1;i<=n;++i){
         for(int j=1;j<=m;++j){
-            dist[i][j] = INT_MAX;
+            if(i == j) dist[i][j] = 0;
+            else dist[i][j] = INT_MAX;
         }
+    }
+
+    for(int i=0;i<m;++i){
+        int u,v,wt;cin>>u>>v>>wt;
+        dist[u][v] = wt;
+    }
+
+    for(int k=1; k<=n;++k){
+        for(int i=1;i<=n;++i){
+            for(int j=1;j<=n;++j){
+                if(dist[i][k] != INT_MAX && dist[k][j] != INT_MAX){
+                    dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j]);
+                }
+            }
+        }
+    }
+
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<=n;++j){
+            if(dist[i][j] == INT_MAX) cout<<"I"<<" ";
+            else cout<<dist[i][j]<<" ";
+        }
+        cout<<"\n";
     }
 }
