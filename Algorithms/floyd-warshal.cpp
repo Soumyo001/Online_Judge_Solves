8f508b2502102b401d05405b544929f73a65fba9
soumyo001 <www.soumyo@gmail.com>
2025-08-16 16:26:30 +0600

    sols

--- Algorithms/floyd-warshal.cpp
+++ Algorithms/floyd-warshal.cpp
@@ -19,7 +19,7 @@ int main(void){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(dist[i][k]!=inf && dist[k][j]!=inf)
-                dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
+                    dist[i][j]=min(dist[i][j],dist[i][k]+dist[k][j]);
             }
         }
     }
