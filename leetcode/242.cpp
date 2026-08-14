9b0061f3b1aa4ff0493360eb439252500ff6c3cd
soumyo001 <www.soumyo@gmail.com>
2026-08-15 01:13:45 +0600

    sidebar & menu files

---
+++ leetcode/242.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+    if(s.length() != t.length()) return false;
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    return s == t;
+}
+
+int main(void) {
+    string a, b;
+    cin >> a >> b;
+    cout<<isAnagram(a, b);
+}
