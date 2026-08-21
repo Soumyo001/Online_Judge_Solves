e521812bc6d89efcfe014e45018d55c42079630d
soumyo001 <www.soumyo@gmail.com>
2026-08-22 00:58:32 +0600

    sidebar & menu files

--- hackerrank/cps_academy_contest/Numbered Grid.cpp
+++ hackerrank/cps_academy_contest/Numbered Grid.cpp
@@ -12,7 +12,7 @@ int main() {
     ll sum = 0;
     for(int i = 0; i < n; ++i) {
         sum += (i * n) + (i + 1);
-        sum += (i * n) + (n - 1 - i) + 1;
+        sum += (i * n) + (n - 1 - i) + 1; // (i*n) = base, rest = offset
     }
     
     if(n & 1) {
